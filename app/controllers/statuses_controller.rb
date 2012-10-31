a86c834a12b259c7beef437fc73e46e2d49f8df3
Riley Hilliard <rileyhilliard@gmail.com>
2012-10-31 09:28:29 -0700

    added gravitars

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -6,7 +6,6 @@ class StatusesController < ApplicationController
   # GET /statuses.json
   def index
     @statuses = Status.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @statuses }
