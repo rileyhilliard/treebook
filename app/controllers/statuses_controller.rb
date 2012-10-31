f7fcd9459b9cd7b10e4d2238e3ab23c5bf6a3a38
Riley Hilliard <rileyhilliard@gmail.com>
2012-10-31 16:18:41 -0700

    fixed another config bug in the statuses_controller

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -7,7 +7,7 @@ class StatusesController < ApplicationController
   def index
     @statuses = Status.all
     respond_to do |format|
-      format.html index.html.erb
+      format.html # index.html.erb
       format.json { render json: @statuses }
     end
   end
